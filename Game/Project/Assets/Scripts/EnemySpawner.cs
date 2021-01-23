42b4409673806fd58bd9d7bf6203bf9492878dd5
JigSawPsycho <michealevans12345@gmail.com>
2021-01-23 19:43:57 +1300

    Spawner first pass implemented

---
+++ Game/Project/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnEnemy), 1f, 1f);
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab, this.gameObject.transform.position, Quaternion.identity, this.transform);
+    }
+}
