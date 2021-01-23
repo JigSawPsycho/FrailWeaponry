21a0f1c9d65905fa1386f34882ab5e6dbb772c0b
JigSawPsycho <michealevans12345@gmail.com>
2021-01-23 21:05:42 +1300

    enemy spawner implemented proto

---
+++ Game/Project/Assets/Scripts/Utility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static float SinGraphY(float x, float q)
+    {
+        return Mathf.Cos(x) + q;
+    }
+}
