7517e366c410ed4b5c680d9490f424aa46aa41fd
Noobcoodoocoo <noobcoodoocoo@gmail.com>
2021-01-24 17:30:34 +1300

    animations and enemy script

---
+++ Game/Project/Assets/deathSoon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathSoon : MonoBehaviour
+{
+    public float wait = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Example2());
+
+
+    }
+
+    IEnumerator Example2()
+    {
+
+        yield return new WaitForSeconds(wait);
+
+
+        Destroy(gameObject);
+    }
+}
---
+++ Game/Project/Assets/enemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public float EnemyHealth = 10f;
+    
+
+    void Update()
+    {
+
+        if (EnemyHealth <= 0f)
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public void GetHit(float damage)
+    {
+        EnemyHealth =- damage;
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+}
